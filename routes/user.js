b8d720b30ff774e2c309fc4711fab1da7732638f
Saqlanekhan <94733426+Saqlanekhan@users.noreply.github.com>
2022-03-30 14:06:36 +0530

    first commit

---
+++ routes/user.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const userController = require('../controller/user');
+const expenseController = require('../controller/expense')
+
+const authenticatemiddleware = require('../middleware/auth');
+
+const router = express.Router();
+
+
+router.post('/signup', userController.signup);
+
+router.post('/login', userController.login)
+
+router.post('/addexpense', expenseController.addexpense )
+
+router.get('/getexpenses',  expenseController.getexpenses )
+
+router.delete('/deleteexpense/:expenseid', expenseController.deleteexpense)
+
+module.exports = router;
